fb7e60dcf76b057813fe25f2d18fee1a0ced75d5
Risky [ Zero Tow ] <santuyaja019@gmail.com>
2021-12-28 16:27:01 +0700

    Add files via upload

---
+++ DMI.py
@@ -0,0 +1,92 @@
+import os,sys
+try: import requests
+except ModuleNotFoundError:print("Sedang Install Module requests");os.system("python -m pip install requests &> /dev/null")
+try: import bs4
+except ModuleNotFoundError:print("Sedang Install Module bs4");os.system("python -m pip install bs4 &> /dev/null")
+try: import mechanize
+except ModuleNotFoundError:print("Sedang Install Module mechanize");os.system("python -m pip install mechanize &> /dev/null")
+
+## import dalam
+import requests as req
+import requests as re
+import time,random,json
+import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,uuid,ipaddress,calendar,requests,mechanize,bs4,sys,os,subprocess,uuid,requests,sys,random,time,re,base64,json,platform
+from datetime import date
+from datetime import datetime
+from time import sleep
+from time import sleep as waktu
+from random import random as acak
+from random import choice as pilih
+from random import randint
+from concurrent.futures import ThreadPoolExecutor as kikygtg
+## import crack
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
+from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor as zthreads
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
+from concurrent.futures import ThreadPoolExecutor as kikygtg
+from requests.exceptions import ConnectionError
+from bs4 import BeautifulSoup as parser
+from bs4 import BeautifulSoup as par
+from requests.exceptions import ConnectionError
+
+#Import Warna
+P = '\x1b[0;97m' # PUTIH
+M = '\x1b[0;91m' # MERAH
+H = '\x1b[0;92m' # HIJAU.
+K = '\x1b[0;93m' # KUNINg.
+B = '\x1b[0;94m' # BIRU.
+U = '\x1b[0;95m' # UNGU.
+O = '\x1b[0;96m' # BIRU MUDA.
+N = '\x1b[0m'    # WARNA MATI
+I='\033[0;32m'
+C='\033[0;36m'
+M='\033[0;31m'
+U='\033[0;35m'
+K='\033[0;33m'
+#P='\033[0;37m'
+P='\033[00m'
+H='\033[0;90m'
+Q="\033[00m"
+i='\033[0;32m'
+c='\033[0;36m'
+m='\033[0;31m'
+u='\033[0;35m'
+k='\033[0;33m'
+b='\033[0;34m'
+#P='\033[0;37m'
+p='\033[00m'
+h='\033[0;90m'
+q="\033[00m"
+war = (f"{C}[{M}•{C}]{Q} ")
+inp = (f"{C}[{M}•{C}]{Q} ")
+bulat = (f"{C}[{M}•{C}]{Q} ")
+
+#Import Text
+def jalan(z):
+	for e in z + "\n":
+		sys.stdout.write(e)
+		sys.stdout.flush()
+		time.sleep(0.02)
+def jalanv1(z):
+	for e in z + "\n":
+		sys.stdout.write(e)
+		sys.stdout.flush()
+		time.sleep(0.0005)
+
+#Import Crack
+loop = 0
+ok = []
+cp = []
+current = datetime.now()
+durasi = str(datetime.now().strftime("%d-%m-%Y"))
+tahun = current.year
+bulan = current.month
+hari = current.day
+current = datetime.now()
+waktuu = str(datetime.now().strftime("%Y-%m-%d"))
+waktu = str(datetime.now().strftime("%Y%m%d"))
+jamz = datetime.now().strftime('%H:%M:%S')
+#bulan_ttl = {"01": "January", "02": "February", "03": "March", "04": "April", "05": "May", "06": "June", "07": "July", "08": "August", "09": "September", "10": "October", "11": "November", "12": "December"}
+bulan_ttl = {"01": "Januari", "02": "Februari", "03": "Maret", "04": "April", "05": "Mei", "06": "Juni", "07": "Juli", "08": "Agustus", "09": "September", "10": "Oktober", "11": "November", "12": "Desember"}
+
